d50459eb14097c964348c7ee58f6e23ebf2962aa
tangchungjiao <2074623967@qq.com>
2023-04-30 10:07:00 +0800

    初始化项目

---
+++ immoc-coupon/coupon-eureka/src/main/java/com/imooc/coupon/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.imooc.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author tangcj
+ * @date 2023/04/29 20:51
+ **/
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
+}
---
+++ immoc-coupon/coupon-gateway/src/main/java/com/imooc/coupon/ZuulGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.imooc.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author tangcj
+ * @date 2023/04/29 20:51
+ **/
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+    }
+}
