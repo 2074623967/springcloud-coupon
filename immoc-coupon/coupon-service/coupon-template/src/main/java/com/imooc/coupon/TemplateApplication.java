36955085e76a90164a8c0480539d55475e878be7
tangchungjiao <2074623967@qq.com>
2023-04-30 20:31:18 +0800

    优惠劵模板微服务搭建

---
+++ immoc-coupon/coupon-service/coupon-template/src/main/java/com/imooc/coupon/TemplateApplication.java
@@ -0,0 +1,23 @@
+package com.imooc.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author tangcj
+ * @date 2023/04/30 20:12
+ * 模版微服务
+ **/
+@EnableScheduling        //开启定时任务
+@EnableJpaAuditing       //审计功能 主动注入创建时间 修改时间
+@EnableEurekaClient      //标识位Eureka Client
+@SpringBootApplication
+public class TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateApplication.class, args);
+    }
+}
