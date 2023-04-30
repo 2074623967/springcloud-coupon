687154a7e1ead1b592544ad1a8a86614cea0fedf
tangchungjiao <2074623967@qq.com>
2023-04-30 11:29:50 +0800

    自定义访问过滤器类

---
+++ immoc-coupon/coupon-gateway/src/main/java/com/imooc/coupon/filter/PreRequestFilter.java
@@ -0,0 +1,29 @@
+package com.imooc.coupon.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author tangcj
+ * @date 2023/04/30 11:29
+ * 在过滤器中存储客户端发起请求的时间戳
+ * 若需要统计用户访问的时间，需配合postFilter
+ **/
+@Slf4j
+@Component
+public class PreRequestFilter extends AbstractPreZuulFilter {
+
+    @Override
+    protected Object cRun() {
+        requestContext.set("startTime", System.currentTimeMillis());
+        return success();
+    }
+
+    /**
+     * 发起请求就执行过滤器，所以优先级最高
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+}
