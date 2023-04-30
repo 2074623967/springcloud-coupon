77c1a93d19e235c955bae455e16205dedf44dac2
tangchungjiao <2074623967@qq.com>
2023-04-30 15:45:34 +0800

    通用配置定义

---
+++ immoc-coupon/coupon-service/coupon-common/src/main/java/com/imooc/coupon/conf/JacksonConfig.java
@@ -0,0 +1,26 @@
+package com.imooc.coupon.conf;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * @author tangcj
+ * @date 2023/04/30 15:37
+ * Jackson的自定义配置
+ **/
+@Configuration
+public class JacksonConfig {
+
+    /**
+     * 让Jackson中的时间类型的字段以yyyy-MM-dd HH:mm:ss格式输出
+     */
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        return mapper;
+    }
+}
