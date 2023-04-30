8519a63968c5f63a3fae0c4c0fbe5cd6736de07e
tangchungjiao <2074623967@qq.com>
2023-04-30 11:18:49 +0800

    自定义Token过滤器类

---
+++ immoc-coupon/coupon-gateway/src/main/java/com/imooc/coupon/filter/TokenFilter.java
@@ -0,0 +1,37 @@
+package com.imooc.coupon.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author tangcj
+ * @date 2023/04/30 11:13
+ * 校验请求中传递的token
+ * 在功能执行之前
+ * 所以使用preFilter
+ **/
+@Slf4j
+@Component
+public class TokenFilter extends AbstractPreZuulFilter {
+
+    @Override
+    protected Object cRun() {
+        //获取request
+        HttpServletRequest request = requestContext.getRequest();
+        log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+
+        String token = request.getParameter("token");
+        if (null == token) {
+            log.error("error: token is empty");
+            return fail(401, "error: token is empty");
+        }
+        return success();
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+}
