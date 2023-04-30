e2695b97b17d9ece8879ddecdae0fdfdbfb6927d
tangchungjiao <2074623967@qq.com>
2023-04-30 20:46:31 +0800

    优惠劵模板相关常量类

---
+++ immoc-coupon/coupon-service/coupon-common/src/main/java/com/imooc/coupon/constant/CouponCategory.java
@@ -0,0 +1,46 @@
+package com.imooc.coupon.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+/**
+ * @author tangcj
+ * @date 2023/04/30 20:12
+ * 优惠券分类
+ **/
+@Getter
+@AllArgsConstructor
+public enum CouponCategory {
+
+    MANJIAN("满减券", "001"),
+    ZHEKOU("折扣券", "002"),
+    LIJIAN("立减券", "003");
+
+    /** 优惠券描述(分类) */
+    //用于返回给用户展示
+    private String description;
+
+    /** 优惠券分类编码 */
+    //主要用于存储
+    private String code;
+
+    /**
+     * 根据code 返回分类枚举
+     * @param code
+     * @return
+     */
+    public static CouponCategory of(String code) {
+        //不允许为空
+        Objects.requireNonNull(code);
+        //对当前枚举过滤
+        //返回与code相同的任意一个枚举值
+        return Stream.of(values())
+                .filter(bean -> bean.code.equals(code))
+                .findAny()//若没有，則抛异常
+                .orElseThrow(() -> new IllegalArgumentException(code + " not exists!"));
+    }
+
+}
