0a691124696783eb89e9a7b4d5981d1aadf97b0f
tangchungjiao <2074623967@qq.com>
2023-05-02 14:31:04 +0800

    搭建优惠劵结算微服务+定义常量类

---
+++ immoc-coupon/coupon-service/coupon-settlement/src/main/java/com/imooc/coupon/SettlementApplication.java
@@ -0,0 +1,19 @@
+package com.imooc.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author tangcj
+ * @date 2023/05/02 14:09
+ * 优惠券结算微服务的启动入口
+ **/
+@EnableEurekaClient
+@SpringBootApplication
+public class SettlementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SettlementApplication.class, args);
+    }
+}
